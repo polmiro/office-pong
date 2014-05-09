c9b195584d0ba72168b1acec7c322487d78f8a4a
Pol Miro <polmiro@gmail.com>
2014-05-08 19:41:50 -0700

    Bang methods in seeds.rb

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -3,8 +3,8 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#   cities = City.create!([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create!(name: 'Emanuel', city: cities.first)
 
 Neo4j::Session.current.query <<-CYPHER
   MATCH (n)
@@ -12,20 +12,20 @@ Neo4j::Session.current.query <<-CYPHER
   DELETE n,r
 CYPHER
 
-pol     = Player.create(:name => "Pol",     :email => "pol@quri.com")
-beau    = Player.create(:name => "Beau",    :email => "beau@quri.com")
-nick    = Player.create(:name => "Nick",    :email => "nick@quri.com")
-matt    = Player.create(:name => "Matt",    :email => "matt@quri.com")
-t2      = Player.create(:name => "T2",      :email => "t2@quri.com")
-brian   = Player.create(:name => "Brian",   :email => "brian@quri.com")
-loic    = Player.create(:name => "Loic",    :email => "loic@quri.com")
-sebas   = Player.create(:name => "Sebas",   :email => "sebastien@quri.com")
-vish    = Player.create(:name => "Vish",    :email => "vish@quri.com")
-brennan = Player.create(:name => "Brennan", :email => "brennan@quri.com")
+pol     = Player.create!(:name => "Pol",     :email => "pol@quri.com")
+beau    = Player.create!(:name => "Beau",    :email => "beau@quri.com")
+nick    = Player.create!(:name => "Nick",    :email => "nick@quri.com")
+matt    = Player.create!(:name => "Matt",    :email => "matt@quri.com")
+t2      = Player.create!(:name => "T2",      :email => "t2@quri.com")
+brian   = Player.create!(:name => "Brian",   :email => "brian@quri.com")
+loic    = Player.create!(:name => "Loic",    :email => "loic@quri.com")
+sebas   = Player.create!(:name => "Sebas",   :email => "sebastien@quri.com")
+vish    = Player.create!(:name => "Vish",    :email => "vish@quri.com")
+brennan = Player.create!(:name => "Brennan", :email => "brennan@quri.com")
 
-engineering = Department.create(:name => "Engineering")
-ops         = Department.create(:name => "Ops")
-sales       = Department.create(:name => "Sales")
+engineering = Department.create!(:name => "Engineering")
+ops         = Department.create!(:name => "Ops")
+sales       = Department.create!(:name => "Sales")
 
 pol.department = engineering
 nick.department = engineering
@@ -44,7 +44,7 @@ GameRule::SETS.product(GameRule::SCORE) do |combination|
   GameRule.create!(:sets => combination[0], :score => combination[1])
 end
 
-tournament = Tournament.create(:title => "I Quri Grand Slam")
+tournament = Tournament.create!(:title => "I Quri Grand Slam")
 Player.all.to_a.each do |player|
   tournament.players << player
   tournament.game_rule = GameRule.find(:sets => 3, :score => 21)
