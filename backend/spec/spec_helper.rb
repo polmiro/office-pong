6d66e39e57c1421ffc7573a5508565638a6a85a5
Pol Miro <polmiro@gmail.com>
2014-05-08 19:58:33 -0700

    Execute specs in order and clean neo4j before each run spec

--- backend/spec/spec_helper.rb
+++ backend/spec/spec_helper.rb
@@ -34,5 +34,13 @@ RSpec.configure do |config|
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
-  config.order = "random"
+  config.order = "1234"
+
+  config.before(:each) do
+    Neo4j::Session.current.query <<-CYPHER
+      MATCH (n)
+      OPTIONAL MATCH (n)-[r]-()
+      DELETE n,r
+    CYPHER
+  end
 end
